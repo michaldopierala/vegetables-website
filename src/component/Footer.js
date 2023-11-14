81037b67f532ed0fc7513fd3f3328dd7967a4918
michaldopierala <michal6452@gmail.com>
2023-11-14 20:40:28 +0100

    final version ready for upload on website

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,13 +4,12 @@ export default function Footer() {
   return (
     <div className='Footer'>
       <div className='adress'>
-        <div className='contact'>contact:</div>
+        <div className='contact'>Contact</div>
         <div className='name'> GreenBean</div>
         <div className='tel'>email: office@green-bean.eu</div>
-        <div className='tel'>tel: +48 885 546 884</div>
+        <div className='tel'>Mobile/Whatsapp: +48 885 546 884</div>
         <div>Jodlowa 16, 55-120 Paniowice, Poland</div>
         <div className='tel'>TAX ident No.: PL6080068114 </div>
-        <div>NIP: 6080068114</div>
       </div>
     </div>
   )
